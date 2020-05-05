f200ad4c6736246b003f9f35499b60404b3561c5
lalit kumar <lalitb4u2@gmail.com>
2020-05-05 17:43:52 +0530

    testcases

--- src/app/todo-using-service/todo-using-service.component.spec.ts
+++ src/app/todo-using-service/todo-using-service.component.spec.ts
@@ -2,15 +2,26 @@ import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { TodoUsingServiceComponent } from "./todo-using-service.component";
 import { FormsModule } from "@angular/forms";
+import { TodoService } from "../todo.service";
 
 describe("TodoUsingServiceComponent", () => {
   let component: TodoUsingServiceComponent;
   let fixture: ComponentFixture<TodoUsingServiceComponent>;
+  let todoServiceStub: Partial<TodoService> = {
+    removeTask: jasmine.createSpy(),
+    addTask: jasmine.createSpy(),
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [FormsModule],
       declarations: [TodoUsingServiceComponent],
+      providers: [
+        {
+          provide: TodoService,
+          useValue: todoServiceStub,
+        },
+      ],
     }).compileComponents();
   }));
 
@@ -22,5 +33,15 @@ describe("TodoUsingServiceComponent", () => {
 
   it("should create", () => {
     expect(component).toBeTruthy();
+  });
+  it("should add the item in list and clear the task name after adding", () => {
+    component.taskName = "coding";
+    component.addTask();
+    expect(todoServiceStub.addTask).toHaveBeenCalledWith("coding");
+    expect(component.taskName).toEqual("");
+  });
+  it("should remove the passed item from the list", () => {
+    component.removeTask("coding");
+    expect(todoServiceStub.removeTask).toHaveBeenCalledWith("coding");
   });
 });
